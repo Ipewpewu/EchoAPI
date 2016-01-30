383698102e23b09319cb7f613f2a933c2f9650d3
FileNotFound <warner.m22@gmail.com>
2016-01-30 14:07:13 -0600

    Added custom config to store service routing

--- EchoAPI/Controllers/EchoController.cs
+++ EchoAPI/Controllers/EchoController.cs
@@ -9,6 +9,8 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using EchoAPI.Models;
 using EchoAPI.Models.Enums;
+using System.Configuration;
+using EchoAPI.Utilities;
 
 namespace EchoAPI.Controllers
 {
@@ -18,28 +20,34 @@ namespace EchoAPI.Controllers
         [HttpPost]
         public dynamic Post(EchoRequest echoRequest)
         {
+            try
+            {
+                //test data for round trip
+                var response = new EchoResponse();
+                response.version = "1.0.1";
+                response.response = new EchoResponse.Response();
+                response.response.shouldEndSession = true;
+                response.response.outputSpeech = new EchoResponse.Response.OutputSpeech();
+                response.response.outputSpeech.type = OutputSpeechType.PlainText;
+                response.response.outputSpeech.text = "Hey! What's Going On.";
+                response.sessionAttributes = ServicesSettings.ServicesCollection[0];
+
+                response.response.card = new EchoResponse.Response.Card();
+                response.response.card.type = CardType.Simple;
+                response.response.card.title = "TestCard";
+                response.response.card.content = "Testing 1 2 3";
+                //response.response.reprompt = new EchoResponse.Response.Reprompt();
+                //response.response.reprompt.outputSpeech = new EchoResponse.Response.OutputSpeech();
+                //response.response.reprompt.outputSpeech.type = "PlainText";
+                //response.response.reprompt.outputSpeech.text = "Say what!";
+
+                return JObject.FromObject(response);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
             
-
-            //test data for round trip
-            var response = new EchoResponse();
-            response.version = "1.0.1";
-            response.response = new EchoResponse.Response();
-            response.response.shouldEndSession = true;
-            response.response.outputSpeech = new EchoResponse.Response.OutputSpeech();
-            response.response.outputSpeech.type = OutputSpeechType.PlainText;
-            response.response.outputSpeech.text = "Hey! What's Going On.";
-            response.sessionAttributes = echoRequest;
-
-            response.response.card = new EchoResponse.Response.Card();
-            response.response.card.type = CardType.Simple;
-            response.response.card.title = "TestCard";
-            response.response.card.content = "Testing 1 2 3";
-            //response.response.reprompt = new EchoResponse.Response.Reprompt();
-            //response.response.reprompt.outputSpeech = new EchoResponse.Response.OutputSpeech();
-            //response.response.reprompt.outputSpeech.type = "PlainText";
-            //response.response.reprompt.outputSpeech.text = "Say what!";
-
-            return JObject.FromObject(response);
         }
     }
 }
---
+++ EchoAPI/Utilities/Services.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoAPI.Utilities
+{
+    public class ServiceSettings : ConfigurationElement
+    {
+        [ConfigurationProperty("id", IsRequired = true)]
+        public string Id
+        {
+            get { return (string)this["id"]; }
+        }
+        [ConfigurationProperty("name", IsRequired = true)]
+        public string Name
+        {
+            get { return (string)this["name"]; }
+        }
+        [ConfigurationProperty("url", IsRequired = true)]
+        public string Url
+        {
+            get { return (string)this["url"]; }
+        }
+        [ConfigurationProperty("port", IsRequired = true)]
+        public string Port
+        {
+            get { return (string)this["port"]; }
+        }
+    }
+    public class ServicesSettings  : ConfigurationSection
+    {
+        [ConfigurationProperty("services", IsDefaultCollection = false)]
+        [ConfigurationCollection(typeof(ServicesCollection), AddItemName = "service")]
+        private ServicesCollection Services
+        {
+            get
+            {
+                return (ServicesCollection)base["services"];
+            }
+        }
+
+        public static ServicesCollection ServicesCollection
+        {
+            get
+            {
+                return ((ServicesSettings)ConfigurationManager.GetSection("servicesSection")).Services;
+            }
+        }
+
+    }
+    public class ServicesCollection : ConfigurationElementCollection
+    {
+        private readonly List<ServiceSettings> elements;
+
+        public ServicesCollection()
+        {
+            this.elements = new List<ServiceSettings>();
+        }
+        public ServiceSettings this[int index]
+        {
+            get { return (ServiceSettings)BaseGet(index); }
+            set
+            {
+                if (BaseGet(index) != null)
+                {
+                    BaseRemoveAt(index);
+                }
+                BaseAdd(index, value);
+            }
+        }
+        protected override ConfigurationElement CreateNewElement()
+        {
+            var element = new ServiceSettings();
+            this.elements.Add(element);
+            return element;
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((ServiceSettings)element).Name;
+        }
+
+    }
+}
